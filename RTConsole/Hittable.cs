fa607f83ea0f82dc2d6182c019ee6afbceb04c2f
David Thibault <david.thibault@sigmund.ca>
2023-04-14 01:29:03 -0400

    6.5 - List of hittable objects

--- RTConsole/Hittable.cs
+++ RTConsole/Hittable.cs
@@ -18,3 +18,32 @@ public abstract class Hittable
 {
     public virtual bool Hit(Ray r, double tMin, double tMax, ref Hit rec) => false;
 }
+
+public class HittableList : Hittable
+{
+    private readonly ICollection<Hittable> _objects;
+
+    public HittableList(ICollection<Hittable> objects)
+    {
+        _objects = objects;
+    }
+
+    public override bool Hit(Ray r, double tMin, double tMax, ref Hit rec)
+    {
+        var tempRec = new Hit();
+        var hitAnything = false;
+        var closestSoFar = tMax;
+
+        foreach (var obj in _objects)
+        {
+            if (obj.Hit(r, tMin, closestSoFar, ref tempRec))
+            {
+                hitAnything = true;
+                closestSoFar = tempRec.T;
+                rec = tempRec;
+            }
+        }
+
+        return hitAnything;
+    }
+}
