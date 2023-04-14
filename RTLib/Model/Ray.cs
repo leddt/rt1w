532a7fcbbf951da95a2694a889776376ea81904e
David Thibault <david.thibault@sigmund.ca>
2023-04-14 17:31:17 -0400

    2.1 - Introduction of spacetime ray tracing

--- RTLib/Model/Ray.cs
+++ RTLib/Model/Ray.cs
@@ -2,14 +2,16 @@
 
 public struct Ray
 {
-    public Ray(Vec3 origin, Vec3 direction)
+    public Ray(Vec3 origin, Vec3 direction, double time = 0)
     {
         Origin = origin;
         Direction = direction;
+        Time = time;
     }
 
     public readonly Vec3 Origin;
     public readonly Vec3 Direction;
+    public readonly double Time;
 
     public Vec3 At(double t) => Origin + t * Direction;
 }
