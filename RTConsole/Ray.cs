c4caa977a8cb7e0f7e80cf269fcd494ba81e4ed9
David Thibault <david.thibault@sigmund.ca>
2023-04-14 00:38:40 -0400

    4.1 - Ray class

---
+++ RTConsole/Ray.cs
@@ -0,0 +1,15 @@
+﻿namespace RTConsole;
+
+public struct Ray
+{
+    public Ray(Vec3 origin, Vec3 direction)
+    {
+        Origin = origin;
+        Direction = direction;
+    }
+
+    public Vec3 Origin { get; }
+    public Vec3 Direction { get; }
+
+    public Vec3 At(double t) => Origin + t * Direction;
+}
