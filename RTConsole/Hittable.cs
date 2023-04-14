195a44db61643d04a01de13dfbc72b10fc86fc88
David Thibault <david.thibault@sigmund.ca>
2023-04-14 01:19:53 -0400

    6.3 - Abstraction for hittable objects

---
+++ RTConsole/Hittable.cs
@@ -0,0 +1,13 @@
+﻿namespace RTConsole;
+
+public struct Hit
+{
+    public Vec3 P;
+    public Vec3 Normal;
+    public double T;
+}
+
+public abstract class Hittable
+{
+    public virtual bool Hit(Ray r, double tMin, double tMax, ref Hit rec) => false;
+}
---
+++ RTConsole/Sphere.cs
@@ -0,0 +1,40 @@
+﻿namespace RTConsole;
+
+public class Sphere : Hittable
+{
+    public Sphere(Vec3 center, double radius)
+    {
+        Center = center;
+        Radius = radius;
+    }
+
+    public Vec3 Center { get; }
+    public double Radius { get; }
+
+    public override bool Hit(Ray r, double tMin, double tMax, ref Hit rec)
+    {
+        var oc = r.Origin - Center;
+        var a = r.Direction.LengthSquared;
+        var halfB = Vec3.Dot(oc, r.Direction);
+        var c = oc.LengthSquared - Radius * Radius;
+
+        var discriminant = halfB * halfB - a * c;
+        if (discriminant < 0) return false;
+        var sqrtD = Math.Sqrt(discriminant);
+
+        // Find the nearest root that lies in the acceptable range.
+        var root = (-halfB - sqrtD) / a;
+        if (root < tMin || tMax < root)
+        {
+            root = (-halfB - sqrtD) / a;
+            if (root < tMin || tMax < root)
+                return false;
+        }
+
+        rec.T = root;
+        rec.P = r.At(rec.T);
+        rec.Normal = (rec.P - Center) / Radius;
+
+        return true;
+    }
+}
