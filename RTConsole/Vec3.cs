48a781ee9f1cd9f3cfe30948fd14866284b81abf
David Thibault <david.thibault@sigmund.ca>
2023-04-14 00:29:50 -0400

    3.2 - Vec3 utility functions

--- RTConsole/Vec3.cs
+++ RTConsole/Vec3.cs
@@ -19,26 +19,23 @@ public struct Vec3
 
     public double Length => Math.Sqrt(LengthSquared);
     public double LengthSquared => X * X + Y * Y + Z * Z;
+
+    public override string ToString() => $"{X} {Y} {Z}";
     
     public double this[int index] => _components[index];
 
-    public static Vec3 operator -(Vec3 a)
-    {
-        return new Vec3(-a.X, -a.Y, -a.Z);
-    }
+    public static Vec3 operator +(Vec3 a, Vec3 b) => new(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+    public static Vec3 operator -(Vec3 a) => new(-a.X, -a.Y, -a.Z);
+    public static Vec3 operator -(Vec3 a, Vec3 b) => new(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+    public static Vec3 operator *(Vec3 vec, double t) => new(vec.X * t, vec.Y * t, vec.Z * t);
+    public static Vec3 operator *(double t, Vec3 vec) => vec * t;
+    public static Vec3 operator /(Vec3 vec, double t) => vec * (1 / t);
     
-    public static Vec3 operator +(Vec3 a, Vec3 b)
-    {
-        return new Vec3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
-    }
-
-    public static Vec3 operator *(Vec3 vec, double t)
-    {
-        return new Vec3(vec.X * t, vec.Y * t, vec.Z * t);
-    }
-
-    public static Vec3 operator /(Vec3 vec, double t)
-    {
-        return vec * (1 / t);
-    }
+    public static double Dot(Vec3 a, Vec3 b) => a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+    public static Vec3 Cross(Vec3 a, Vec3 b) => new Vec3(
+        a.Y * b.Z - a.Z * b.Y,
+        a.Z * b.X - a.X * b.Z,
+        a.X * b.Y - a.Y * b.X
+    );
+    public static Vec3 UnitVector(Vec3 v) => v / v.Length;
 }
