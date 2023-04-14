23566b2276c33f8b68aae56ab9fd8f19b313eea3
David Thibault <david.thibault@sigmund.ca>
2023-04-14 00:51:18 -0400

    4.2 - Sending rays into the scene

--- RTConsole/Program.cs
+++ RTConsole/Program.cs
@@ -1,7 +1,21 @@
 ﻿using RTConsole;
 
-const int imageWidth = 256;
-const int imageHeight = 256;
+// Image
+const double aspectRatio = 16.0 / 9.0;
+const int imageWidth = 400;
+const int imageHeight = (int)(imageWidth / aspectRatio);
+
+// Camera
+var viewportHeight = 2.0;
+var viewportWidth = aspectRatio * viewportHeight;
+var focalLength = 1.0;
+
+var origin = new Vec3(0, 0, 0);
+var horizontal = new Vec3(viewportWidth, 0, 0);
+var vertical = new Vec3(0, viewportHeight, 0);
+var lowerLeftCorner = origin - horizontal / 2 - vertical / 2 - new Vec3(0, 0, focalLength);
+
+// Render
 
 Console.Write($"P3\n{imageWidth} {imageHeight}\n255\n");
 
@@ -10,9 +24,20 @@ for (var j = imageHeight - 1; j >= 0; j--)
     Console.Error.Write($"\rScanlines remaining: {j} ");
     for (var i = 0; i < imageWidth; i++)
     {
-        var pixelColor = new Vec3((double)i / (imageWidth - 1), (double)j / (imageHeight - 1), .25);
+        var u = (double)i / (imageWidth - 1);
+        var v = (double)j / (imageHeight - 1);
+        var r = new Ray(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
+        
+        var pixelColor = RayColor(r);
         pixelColor.WriteColor(Console.Out);
     }
 }
 
 Console.Error.Write("\nDone.\n");
+
+Vec3 RayColor(Ray r)
+{
+    var unitDirection = Vec3.UnitVector(r.Direction);
+    var t = 0.5 * (unitDirection.Y + 1);
+    return (1 - t) * new Vec3(1, 1, 1) + t * new Vec3(0.5, 0.7, 1);
+}
