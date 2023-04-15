74dce75bd2fe17f51b3c92f3d6124863e75231d8
David Thibault <david.thibault@sigmund.ca>
2023-04-15 14:58:34 -0400

    8.1 - Instance translation

---
+++ RTLib/Hittables/Translate.cs
@@ -0,0 +1,40 @@
+﻿using RTLib.Model;
+
+namespace RTLib.Hittables;
+
+public class Translate : IHittable
+{
+    private readonly IHittable _hittable;
+    private readonly Vec3 _displacement;
+
+    public Translate(IHittable hittable, Vec3 displacement)
+    {
+        _hittable = hittable;
+        _displacement = displacement;
+    }
+
+    public bool Hit(Ray r, double tMin, double tMax, ref Hit rec)
+    {
+        var movedRay = new Ray(r.Origin - _displacement, r.Direction, r.Time);
+        if (!_hittable.Hit(movedRay, tMin, tMax, ref rec))
+            return false;
+
+        rec.P += _displacement;
+        rec.SetFaceNormal(movedRay, rec.Normal);
+
+        return true;
+    }
+
+    public bool GetBoundingBox(double time0, double time1, out BoundingBox output)
+    {
+        if (!_hittable.GetBoundingBox(time0, time1, out output))
+            return false;
+
+        output = new BoundingBox(
+            output.Minimum + _displacement,
+            output.Maximum + _displacement
+        );
+
+        return true;
+    }
+}
