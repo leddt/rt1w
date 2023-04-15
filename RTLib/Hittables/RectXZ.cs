aab40de03bea88c00f01b6ed1014adca2753a0d5
David Thibault <david.thibault@sigmund.ca>
2023-04-15 14:35:02 -0400

    7.5 - More axis-aligned rectangles

---
+++ RTLib/Hittables/RectXZ.cs
@@ -0,0 +1,49 @@
+﻿using RTLib.Materials;
+using RTLib.Model;
+
+namespace RTLib.Hittables;
+
+public class RectXZ : IHittable
+{
+    private readonly double _x0, _x1, _z0, _z1, _k;
+    private readonly IMaterial _material;
+
+    public RectXZ(double x0, double x1, double z0, double z1, double k, IMaterial material)
+    {
+        _x0 = x0;
+        _x1 = x1;
+        _z0 = z0;
+        _z1 = z1;
+        _k = k;
+        _material = material;
+    }
+
+    public bool Hit(Ray r, double tMin, double tMax, ref Hit rec)
+    {
+        var t = (_k - r.Origin.Y) / r.Direction.Y;
+        if (t < tMin || t > tMax)
+            return false;
+
+        var x = r.Origin.X + t * r.Direction.X;
+        var z = r.Origin.Z + t * r.Direction.Z;
+        if (x < _x0 || x > _x1 || z < _z0 || z > _z1)
+            return false;
+
+        rec.U = (x - _x0) / (_x1 - _x0);
+        rec.V = (z - _z0) / (_z1 - _z0);
+        rec.T = t;
+
+        var outwardNormal = new Vec3(0, 1, 0);
+        rec.SetFaceNormal(r, outwardNormal);
+        rec.Material = _material;
+        rec.P = r.At(t);
+        return true;
+    }
+
+    public bool GetBoundingBox(double time0, double time1, out BoundingBox output)
+    {
+        // The bounding box must have non-zero width in each dimension, so pad the Y dimension a small amount.
+        output = new BoundingBox(new Vec3(_x0, _k - 0.0001, _z0), new Vec3(_x1, _k + 0.0001, _z1));
+        return true;
+    }
+}
---
+++ RTLib/Hittables/RectYZ.cs
@@ -0,0 +1,49 @@
+﻿using RTLib.Materials;
+using RTLib.Model;
+
+namespace RTLib.Hittables;
+
+public class RectYZ : IHittable
+{
+    private readonly double _y0, _y1, _z0, _z1, _k;
+    private readonly IMaterial _material;
+
+    public RectYZ(double y0, double y1, double z0, double z1, double k, IMaterial material)
+    {
+        _y0 = y0;
+        _y1 = y1;
+        _z0 = z0;
+        _z1 = z1;
+        _k = k;
+        _material = material;
+    }
+
+    public bool Hit(Ray r, double tMin, double tMax, ref Hit rec)
+    {
+        var t = (_k - r.Origin.X) / r.Direction.X;
+        if (t < tMin || t > tMax)
+            return false;
+
+        var y = r.Origin.Y + t * r.Direction.Y;
+        var z = r.Origin.Z + t * r.Direction.Z;
+        if (y < _y0 || y > _y1 || z < _z0 || z > _z1)
+            return false;
+
+        rec.U = (y - _y0) / (_y1 - _y0);
+        rec.V = (z - _z0) / (_z1 - _z0);
+        rec.T = t;
+
+        var outwardNormal = new Vec3(1, 0, 0);
+        rec.SetFaceNormal(r, outwardNormal);
+        rec.Material = _material;
+        rec.P = r.At(t);
+        return true;
+    }
+
+    public bool GetBoundingBox(double time0, double time1, out BoundingBox output)
+    {
+        // The bounding box must have non-zero width in each dimension, so pad the X dimension a small amount.
+        output = new BoundingBox(new Vec3(_k - 0.0001, _y0, _z0), new Vec3(_k + 0.0001, _y1, _z1));
+        return true;
+    }
+}
