65a462b52ec39f279746a2cea7dd131935664563
David Thibault <david.thibault@sigmund.ca>
2023-04-15 19:55:48 -0400

    10 - Final image

--- RTConsole/Program.cs
+++ RTConsole/Program.cs
@@ -12,8 +12,8 @@ var commandLine = Environment.GetCommandLineArgs();
 
 var renderSettings = new RenderSettings(
     aspectRatio: 1, // 16.0 / 9.0,
-    imageWidth: 600,
-    samplesPerPixel: 400,
+    imageWidth: 800,
+    samplesPerPixel: 10000,
     maxDepth: 50
 );
 
@@ -142,7 +142,6 @@ switch (sceneIndex)
     }
     
     case 7:
-    default:
     {
         var lookFrom = new Vec3(278, 278, -800);
         var lookAt = new Vec3(278, 278, 0);
@@ -157,6 +156,26 @@ switch (sceneIndex)
             0, 1);
 
         scene = CornellSmoke();
+        background = new Vec3(0, 0, 0);
+        break;
+    }
+
+    case 8:
+    default:
+    {
+        var lookFrom = new Vec3(478, 278, -600);
+        var lookAt = new Vec3(278, 278, 0);
+        camera = new Camera(
+            lookFrom,
+            lookAt,
+            vUp: new Vec3(0, 1, 0),
+            vfov: 40,
+            renderSettings.AspectRatio,
+            aperture: 0.1,
+            focusDist: 600,
+            0, 1);
+        
+        scene = FinalScene();
         background = new Vec3(0, 0, 0);
         break;
     }
@@ -327,15 +346,71 @@ IHittable CornellSmoke()
     var box1 = new Box(new Vec3(0, 0, 0), new Vec3(165, 330, 165), white)
         .RotateY(15)
         .Translate(265, 0, 295);
+    world.Add(new ConstantMedium(box1, 0.01, new Vec3(0, 0, 0)));
 
     var box2 = new Box(new Vec3(0, 0, 0), new Vec3(165, 165, 165), white)
         .RotateY(-18)
         .Translate(130, 0, 65);
-
-    world.Add(new ConstantMedium(box1, 0.01, new Vec3(0, 0, 0)));
     world.Add(new ConstantMedium(box2, 0.01, new Vec3(1, 1, 1)));
 
     return world;
+}
+
+IHittable FinalScene()
+{
+    var boxes1 = new HittableList();
+    var ground = new Lambertian(new Vec3(0.48, 0.83, 0.53));
+
+    var boxesPerSide = 20;
+    for (var i = 0; i < boxesPerSide; i++)
+    for (var j = 0; j < boxesPerSide; j++)
+    {
+        var w = 100.0;
+        var x0 = -1000 + i * w;
+        var z0 = -1000 + j * w;
+        var y0 = 0;
+        var x1 = x0 + w;
+        var y1 = Random.Shared.NextDouble(1, 101);
+        var z1 = z0 + w;
+
+        boxes1.Add(new Box(new Vec3(x0, y0, z0), new Vec3(x1, y1, z1), ground));
+    }
+
+    var objects = new HittableList();
+    
+    objects.Add(new BvhNode(boxes1, 0, 1));
+
+    var light = new DiffuseLight(new Vec3(7, 7, 7));
+    objects.Add(new RectXZ(123, 423, 147, 412, 554, light));
+
+    var center1 = new Vec3(400, 400, 200);
+    var center2 = center1 + new Vec3(30, 0, 0);
+    var movingSphereMaterial = new Lambertian(new Vec3(0.7, 0.3, 0.1));
+    objects.Add(new MovingSphere(center1, center2, 0, 1, 50, movingSphereMaterial));
+
+    objects.Add(new Sphere(new Vec3(260, 150, 45), 50, new Dielectric(1.5)));
+    objects.Add(new Sphere(new Vec3(0, 150, 145), 50, new Metal(new Vec3(0.8, 0.8, 0.9), 1)));
+
+    var boundary = new Sphere(new Vec3(360, 150, 145), 70, new Dielectric(1.5));
+    objects.Add(boundary);
+    objects.Add(new ConstantMedium(boundary, 0.2, new Vec3(0.2, 0.4, 0.9)));
+    boundary = new Sphere(new Vec3(0, 0, 0), 5000, new Dielectric(1.5));
+    objects.Add(new ConstantMedium(boundary, .0001, new Vec3(1, 1, 1)));
+
+    var emat = new Lambertian(new ImageTexture("Textures/earthmap.jpg"));
+    objects.Add(new Sphere(new Vec3(400, 200, 400), 100, emat));
+    var pertext = new NoiseTexture(0.1);
+    objects.Add(new Sphere(new Vec3(220, 280, 300), 80, new Lambertian(pertext)));
+
+    var boxes2 = new HittableList();
+    var white = new Lambertian(new Vec3(0.73, 0.73, 0.73));
+    var ns = 1000;
+    for (var j = 0; j < ns; j++) 
+        boxes2.Add(new Sphere(Vec3.Random(0, 165), 10, white));
+
+    objects.Add(new BvhNode(boxes2, 0, 1).RotateY(15).Translate(-100, 270, 395));
+
+    return objects;
 }
 
 Stream GetOutputStream(out IFormat format)
