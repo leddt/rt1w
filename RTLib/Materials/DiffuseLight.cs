8e97f192337d718bbadcf9283afe6390747ab24e
David Thibault <david.thibault@sigmund.ca>
2023-04-15 13:53:22 -0400

    7.1 - Emissive materials

---
+++ RTLib/Materials/DiffuseLight.cs
@@ -0,0 +1,30 @@
+﻿using RTLib.Model;
+using RTLib.Textures;
+
+namespace RTLib.Materials;
+
+public class DiffuseLight : IMaterial
+{
+    private readonly ITexture _texture;
+
+    public DiffuseLight(Vec3 color) : this(new SolidColor(color))
+    {
+    }
+
+    public DiffuseLight(ITexture texture)
+    {
+        _texture = texture;
+    }
+
+    public bool Scatter(Ray rIn, Hit rec, out Vec3 attenuation, out Ray scattered)
+    {
+        attenuation = new Vec3();
+        scattered = new Ray();
+        return false;
+    }
+
+    public Vec3 Emitted(double u, double v, Vec3 p)
+    {
+        return _texture.Value(u, v, p);
+    }
+}
--- RTLib/Materials/IMaterial.cs
+++ RTLib/Materials/IMaterial.cs
@@ -5,4 +5,5 @@ namespace RTLib.Materials;
 public interface IMaterial
 {
     bool Scatter(Ray rIn, Hit rec, out Vec3 attenuation, out Ray scattered);
+    Vec3 Emitted(double u, double v, Vec3 p) => new();
 }
