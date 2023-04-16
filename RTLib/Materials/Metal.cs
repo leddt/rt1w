d0d228f9e70e5ea18a5d9fc4804b4277ff5c6d58
David Thibault <david.thibault@sigmund.ca>
2023-04-16 02:04:55 -0400

    Metal albedo can be a texture

--- RTLib/Materials/Metal.cs
+++ RTLib/Materials/Metal.cs
@@ -1,13 +1,17 @@
 ﻿using RTLib.Model;
+using RTLib.Textures;
 
 namespace RTLib.Materials;
 
 public class Metal : IMaterial
 {
-    private readonly Vec3 _albedo;
+    private readonly ITexture _albedo;
     private readonly double _fuzz;
 
-    public Metal(Vec3 albedo, double fuzz)
+    public Metal(Vec3 albedo, double fuzz) : this (new SolidColor(albedo), fuzz)
+    {
+    }
+    public Metal(ITexture albedo, double fuzz)
     {
         _albedo = albedo;
         _fuzz = fuzz;
@@ -17,7 +21,7 @@ public class Metal : IMaterial
     {
         var reflected = rIn.Direction.UnitVector().Reflect(rec.Normal);
         scattered = new Ray(rec.P, reflected + _fuzz * Vec3.RandomInUnitSphere(), rIn.Time);
-        attenuation = _albedo;
+        attenuation = _albedo.Value(rec.U, rec.V, rec.P);
         return Vec3.Dot(scattered.Direction, rec.Normal) > 0;
     }
 }
